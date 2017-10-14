c02c36ef168e13d3b1c29125d52c11a6d74ca629
budiadiono <budiadiono@gmail.com>
2017-10-14 11:25:44 +0700

    false 'gen' command bug fix

--- bin/lempit.js
+++ bin/lempit.js
@@ -6,10 +6,10 @@ program
   .version(require("../package").version)
   .usage("<command> [options]")
   .command("init", "generate a new project from a template")
-  .command("gen", "generate a new file(s) from a template directory or file stored in `.lempit` directory.")
+  .command("new", "generate a new file(s) from a template directory or file stored in `.lempit` directory.")
   .parse(process.argv);
 
 // whitelisting args
-if (["init", "gen"].indexOf(program.args[0]) < 0) {
+if (["init", "new"].indexOf(program.args[0]) < 0) {
   return program.help();
 }
